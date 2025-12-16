de720537652106688921410367b11b221698111b
bong-woojin <bongwoojin1993@gmail.com>
2025-12-17 00:02:07 +0900

    crud연습

---
+++ static/app.js
@@ -0,0 +1,25 @@
+async function createMemo(value) {
+  //async랑 await랑 셋트
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date(),
+      content: value,
+    }),
+  });
+  const jsonRes = await res.json();
+  console.log(jsonRes);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", handleSubmit);
